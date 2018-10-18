8d3ffdf8f85adf252dfb918b6b0bd1fda70ceb15
himanscience <himanshugholap96@gmail.com>
2018-10-18 22:07:19 +0530

    webpages

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>martialarts.org</title>
+		<link rel = 'stylesheet' href = './styles/bootstrap.min.css' />
+		<style type="text/css">
+			body {
+				background-image: url('stars.jpg');
+        
+			}
+			table{
+				color: yellow;
+			}
+		</style>
+	</head>
+	<body>
+	<?php include 'martial.html';?>
+
+	
+	<script src = './scripts/jquery-2.1.4.min.js'></script>
+	<script src = './scripts/bootstrap.min.js' ></script>
+	</body>
+</html>
